590d92eb744400d473be562074583b4144386371
Juan Rojas <jaunmanuel2005@gmail.com>
2024-09-22 04:53:54 -0500

    Login

--- co.edu.uniquindio.finalprojectfx/src/main/java/co/edu/uniquindio/finalprojectfx/finalprojectapp/controller/LoginController.java
+++ co.edu.uniquindio.finalprojectfx/src/main/java/co/edu/uniquindio/finalprojectfx/finalprojectapp/controller/LoginController.java
@@ -1,4 +1,32 @@
 package co.edu.uniquindio.finalprojectfx.finalprojectapp.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField txtContrasena;
+
+    @FXML
+    private Button btnIngresar;
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    void initialize() {
+        assert txtContrasena != null : "fx:id=\"txtContrasena\" was not injected: check your FXML file 'Login.fxml'.";
+        assert btnIngresar != null : "fx:id=\"btnIngresar\" was not injected: check your FXML file 'Login.fxml'.";
+        assert txtUsuario != null : "fx:id=\"txtUsuario\" was not injected: check your FXML file 'Login.fxml'.";
+    }
 }
