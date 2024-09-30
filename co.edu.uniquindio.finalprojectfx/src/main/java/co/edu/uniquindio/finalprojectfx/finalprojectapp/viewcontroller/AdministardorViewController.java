ee433a768b0af6e95997ec8a8c7ef0f5a0382dd4
juan-rojas-afk <159515437+juan-rojas-afk@users.noreply.github.com>
2024-09-30 02:45:10 -0500

    ViewController

---
+++ co.edu.uniquindio.finalprojectfx/src/main/java/co/edu/uniquindio/finalprojectfx/finalprojectapp/viewcontroller/AdministardorViewController.java
@@ -0,0 +1,127 @@
+package co.edu.uniquindio.finalprojectfx.finalprojectapp.viewcontroller;
+
+import java.net.URL;
+import java.net.http.WebSocket;
+import java.util.ResourceBundle;
+
+import co.edu.uniquindio.finalprojectfx.finalprojectapp.controller.VendedorController;
+import co.edu.uniquindio.finalprojectfx.finalprojectapp.mapping.dto.VendedorDto;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class AdministardorViewController {
+
+    VendedorController vendedorController;
+    ObservableList<VendedorDto> listaVendedores = FXCollections.observableArrayList();
+    VendedorDto vendedorSeleccionado;
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField TxtCedula;
+
+    @FXML
+    private Button btnActualizar;
+
+    @FXML
+    private Button btnAgregar;
+
+    @FXML
+    private Button btnNuevo;
+
+    @FXML
+    private Button btneEliminar;
+
+    @FXML
+    private TableView<VendedorDto> tableVendedor;
+
+    @FXML
+    private TableColumn<VendedorDto, String> tcApellidos;
+
+    @FXML
+    private TableColumn<VendedorDto, String> tcCedula;
+
+    @FXML
+    private TableColumn<VendedorDto, String> tcContrasena;
+
+    @FXML
+    private TableColumn<VendedorDto, String> tcDireccion;
+
+    @FXML
+    private TableColumn<VendedorDto, String> tcNombre;
+
+    @FXML
+    private TableColumn<VendedorDto, String> tcUsuario;
+
+    @FXML
+    private TextField txtApellidos;
+
+    @FXML
+    private TextField txtDireccion;
+
+    @FXML
+    private TextField txtNombre;
+
+    @FXML
+    private TextField txtNombre1;
+
+    @FXML
+    private TextField txtNombre2;
+
+    @FXML
+    void initialize() {
+        vendedorController = new VendedorController();
+        initView();
+    }
+
+    private void initView() {
+        initDataBinding();
+        obtenerVendedor();
+        tableVendedor.getItems().clear();
+        tableVendedor.setItems(listaVendedores);
+        listenerSelection();
+    }
+
+    private void obtenerVendedor() {
+
+    }
+
+
+    private void initDataBinding() {
+        tcNombre.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().nombre()));
+        tcApellidos.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().apellidos()));
+        tcCedula.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().cedula()));
+        tcDireccion.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().direccion()));
+        tcUsuario.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().usuario()));
+        tcContrasena.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().contrasena()));
+    }
+
+    private void listenerSelection() {
+        tableVendedor.getSelectionModel().selectedItemProperty().addListener((obs, oldSelection, newSelection) -> {
+            vendedorSeleccionado = newSelection;
+            mostrarInformacionVendedor(vendedorSeleccionado);
+        });
+    }
+
+    private void mostrarInformacionVendedor(VendedorDto vendedorSeleccionado) {
+        if(vendedorSeleccionado != null){
+            txtNombre.setText(vendedorSeleccionado.nombre());
+            txtApellidos.setText(vendedorSeleccionado.apellidos());
+            TxtCedula.setText(vendedorSeleccionado.cedula());
+            txtDireccion.setText(vendedorSeleccionado.direccion());
+        }
+    }
+
+}
+
