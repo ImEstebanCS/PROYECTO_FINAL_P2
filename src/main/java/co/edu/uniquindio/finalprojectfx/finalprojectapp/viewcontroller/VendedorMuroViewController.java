4d796eae212acfe23bf038f54c1b0b55ae29d373
Maicol Paez Cruz <maicols.paezc@uqvirtual.edu.co>
2024-11-12 13:19:09 -0500

    cargando interfaz

---
+++ src/main/java/co/edu/uniquindio/finalprojectfx/finalprojectapp/viewcontroller/VendedorMuroViewController.java
@@ -0,0 +1,22 @@
+package co.edu.uniquindio.finalprojectfx.finalprojectapp.viewcontroller;
+
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VendedorMuroViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
+
+
